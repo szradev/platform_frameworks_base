5d688d44824452b1e9b6fac7c01d11785f701889
szradev <gronkibronka@gmail.com>
2021-04-15 22:34:30 +0200

    Added screenshot sound switcher [1\2]

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -744,11 +744,10 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
                     }
                 });
 
-                if (Settings.System.getIntForUser(mContext.getContentResolver(),
-                    Settings.System.SCREENSHOT_SHUTTER_SOUND, 1, UserHandle.USER_CURRENT) == 1) {
-                    // Play the shutter sound to notify that we've taken a screenshot
-                    mCameraSound.play(MediaActionSound.SHUTTER_CLICK);
-                }
+                if (Settings.System.getInt(mContext.getContentResolver(),
+                      Settings.System.SCREENSHOT_SOUND, 0) == 1) {
+                  mCameraSound.play(MediaActionSound.SHUTTER_CLICK);
+               }
 
                 mScreenshotPreview.setLayerType(View.LAYER_TYPE_HARDWARE, null);
                 mScreenshotPreview.buildLayer();
